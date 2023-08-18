2a4eb1218913e2cebf0dafb83a517bd2b71dad40
Miishant <137567648+Miishant@users.noreply.github.com>
2023-08-18 09:16:27 -0600

    Add files via upload

---
+++ Exam/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // Load header
+    $('#global-header').load('header.html', function() {
+        // Header loaded callback, if needed
+    });
+
+    // Load footer
+    $('#global-footer').load('footer.html', function() {
+        // Footer loaded callback, if needed
+    });
+    
+});
